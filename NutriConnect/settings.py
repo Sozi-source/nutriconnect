57a5afe2ebaeb9923e0594dffe6377bfbfa42553
Wilfred Osozi <osoziw@gmail.com>
2026-02-19 07:35:32 +0000

    updated admin.py

--- NutriConnect/settings.py
+++ NutriConnect/settings.py
@@ -134,3 +134,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
